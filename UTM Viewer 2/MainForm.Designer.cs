fe213405ce778141d087b448558d123c256f030f
nda86 <nda86@mail.ru>
2016-05-09 02:31:34 +0800

    добавил форму для переотправку ТТН,

--- UTM Viewer 2/MainForm.Designer.cs
+++ UTM Viewer 2/MainForm.Designer.cs
@@ -42,12 +42,20 @@
             this.colComment = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.btnGetFormBRegInfo = new System.Windows.Forms.Button();
             this.txtFormBRegInfo = new System.Windows.Forms.RichTextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabViewer1 = new System.Windows.Forms.TabPage();
+            this.tabSender = new System.Windows.Forms.TabPage();
+            this.btnSendResend = new System.Windows.Forms.Button();
+            this.txtOutTTN = new System.Windows.Forms.RichTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.tableTicket)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabViewer1.SuspendLayout();
+            this.tabSender.SuspendLayout();
             this.SuspendLayout();
             // 
             // txtIPUTM
             // 
-            this.txtIPUTM.Location = new System.Drawing.Point(87, 16);
+            this.txtIPUTM.Location = new System.Drawing.Point(361, 5);
             this.txtIPUTM.Name = "txtIPUTM";
             this.txtIPUTM.Size = new System.Drawing.Size(118, 20);
             this.txtIPUTM.TabIndex = 0;
@@ -56,7 +64,7 @@
             // 
             this.label1.BackColor = System.Drawing.Color.Silver;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Location = new System.Drawing.Point(285, 5);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(77, 19);
             this.label1.TabIndex = 1;
@@ -65,7 +73,7 @@
             // 
             // btnGetAllTTN
             // 
-            this.btnGetAllTTN.Location = new System.Drawing.Point(211, 16);
+            this.btnGetAllTTN.Location = new System.Drawing.Point(743, 390);
             this.btnGetAllTTN.Name = "btnGetAllTTN";
             this.btnGetAllTTN.Size = new System.Drawing.Size(75, 21);
             this.btnGetAllTTN.TabIndex = 2;
@@ -88,9 +96,9 @@
             this.colOperation,
             this.colOperationResult,
             this.colComment});
-            this.tableTicket.Location = new System.Drawing.Point(12, 43);
+            this.tableTicket.Location = new System.Drawing.Point(3, 3);
             this.tableTicket.Name = "tableTicket";
-            this.tableTicket.Size = new System.Drawing.Size(617, 412);
+            this.tableTicket.Size = new System.Drawing.Size(825, 381);
             this.tableTicket.TabIndex = 4;
             // 
             // colIP
@@ -154,31 +162,81 @@
             // 
             // btnGetFormBRegInfo
             // 
-            this.btnGetFormBRegInfo.Location = new System.Drawing.Point(635, 15);
+            this.btnGetFormBRegInfo.Location = new System.Drawing.Point(7, 9);
             this.btnGetFormBRegInfo.Name = "btnGetFormBRegInfo";
-            this.btnGetFormBRegInfo.Size = new System.Drawing.Size(75, 21);
+            this.btnGetFormBRegInfo.Size = new System.Drawing.Size(188, 21);
             this.btnGetFormBRegInfo.TabIndex = 5;
-            this.btnGetFormBRegInfo.Text = "GET";
+            this.btnGetFormBRegInfo.Text = "Get All TTN";
             this.btnGetFormBRegInfo.UseVisualStyleBackColor = true;
             // 
             // txtFormBRegInfo
             // 
-            this.txtFormBRegInfo.Location = new System.Drawing.Point(635, 43);
+            this.txtFormBRegInfo.Location = new System.Drawing.Point(7, 36);
             this.txtFormBRegInfo.Name = "txtFormBRegInfo";
-            this.txtFormBRegInfo.Size = new System.Drawing.Size(188, 412);
+            this.txtFormBRegInfo.Size = new System.Drawing.Size(188, 364);
             this.txtFormBRegInfo.TabIndex = 6;
             this.txtFormBRegInfo.Text = "";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabViewer1);
+            this.tabControl1.Controls.Add(this.tabSender);
+            this.tabControl1.Location = new System.Drawing.Point(1, 24);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(836, 444);
+            this.tabControl1.TabIndex = 7;
+            // 
+            // tabViewer1
+            // 
+            this.tabViewer1.Controls.Add(this.tableTicket);
+            this.tabViewer1.Controls.Add(this.btnGetAllTTN);
+            this.tabViewer1.Location = new System.Drawing.Point(4, 22);
+            this.tabViewer1.Name = "tabViewer1";
+            this.tabViewer1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabViewer1.Size = new System.Drawing.Size(828, 418);
+            this.tabViewer1.TabIndex = 0;
+            this.tabViewer1.Text = "Viewer1";
+            this.tabViewer1.UseVisualStyleBackColor = true;
+            // 
+            // tabSender
+            // 
+            this.tabSender.Controls.Add(this.txtOutTTN);
+            this.tabSender.Controls.Add(this.btnSendResend);
+            this.tabSender.Controls.Add(this.btnGetFormBRegInfo);
+            this.tabSender.Controls.Add(this.txtFormBRegInfo);
+            this.tabSender.Location = new System.Drawing.Point(4, 22);
+            this.tabSender.Name = "tabSender";
+            this.tabSender.Padding = new System.Windows.Forms.Padding(3);
+            this.tabSender.Size = new System.Drawing.Size(828, 418);
+            this.tabSender.TabIndex = 1;
+            this.tabSender.Text = "Sender";
+            this.tabSender.UseVisualStyleBackColor = true;
+            // 
+            // btnSendResend
+            // 
+            this.btnSendResend.Location = new System.Drawing.Point(286, 9);
+            this.btnSendResend.Name = "btnSendResend";
+            this.btnSendResend.Size = new System.Drawing.Size(188, 21);
+            this.btnSendResend.TabIndex = 7;
+            this.btnSendResend.Text = "Resend TTN";
+            this.btnSendResend.UseVisualStyleBackColor = true;
+            // 
+            // txtOutTTN
+            // 
+            this.txtOutTTN.Location = new System.Drawing.Point(286, 36);
+            this.txtOutTTN.Name = "txtOutTTN";
+            this.txtOutTTN.Size = new System.Drawing.Size(188, 364);
+            this.txtOutTTN.TabIndex = 8;
+            this.txtOutTTN.Text = "";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(835, 467);
-            this.Controls.Add(this.txtFormBRegInfo);
-            this.Controls.Add(this.btnGetFormBRegInfo);
-            this.Controls.Add(this.tableTicket);
-            this.Controls.Add(this.btnGetAllTTN);
+            this.ClientSize = new System.Drawing.Size(835, 458);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.txtIPUTM);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "MainForm";
@@ -186,6 +244,9 @@
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.Resize += new System.EventHandler(this.MainForm_Resize);
             ((System.ComponentModel.ISupportInitialize)(this.tableTicket)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabViewer1.ResumeLayout(false);
+            this.tabSender.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -207,6 +268,11 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn colComment;
         private System.Windows.Forms.Button btnGetFormBRegInfo;
         private System.Windows.Forms.RichTextBox txtFormBRegInfo;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabViewer1;
+        private System.Windows.Forms.TabPage tabSender;
+        private System.Windows.Forms.RichTextBox txtOutTTN;
+        private System.Windows.Forms.Button btnSendResend;
     }
 }
 
